d76e18eea1afc6a0696c68bc814411ceb6f6970d
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2023-08-08 23:09:07 +0000

    file extension organizer

---
+++ script.py
@@ -0,0 +1,36 @@
+import os
+import shutil
+
+class InvalidFolderPathError(Exception):
+    pass
+
+def is_folder_path(path):
+    if not os.path.isdir(path):
+        raise InvalidFolderPathError("Invalid folder path")
+
+loop = True
+
+while loop:
+    folder_path = input("Enter folder path: ")
+
+    try:
+        is_folder_path(folder_path)
+        print("Valid folder path!")
+        loop = False
+    except InvalidFolderPathError as e:
+        print(e)
+
+
+for file in os.listdir(folder_path):
+    file_path = os.path.join(folder_path,file)
+    if os.path.isfile(file_path):
+        extension = os.path.splitext(file)[1]
+        extension_path  = os.path.join(folder_path,extension)
+        
+        if os.path.exists(extension_path):
+            shutil.move(file_path,extension_path)
+        
+        else:
+            os.mkdir(extension_path)
+            shutil.move(file_path,extension_path)
+        
